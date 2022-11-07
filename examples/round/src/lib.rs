411f9e924f97b9b1022f0e0a65af5f27df9a4c15
xiyao <futuretech6@foxmail.com>
2022-11-07 14:39:11 +0800

    feat: add round example

---
+++ examples/round/src/lib.rs
@@ -0,0 +1,32 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::U128;
+use near_sdk::near_bindgen;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    bonus_rate: u8,
+    fee_rate: u8,
+}
+
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            bonus_rate: 2,
+            fee_rate: 1,
+        }
+    }
+}
+
+#[near_bindgen]
+impl Contract {
+    pub fn calc_bonus(&self, amount: U128) -> U128 {
+        // don't use `round`, you should use `ceil` here
+        U128((amount.0 as f64 * self.bonus_rate as f64 / 100.).round() as u128)
+    }
+
+    pub fn calc_fee(&self, amount: U128) -> U128 {
+        // don't use `round`, you should use `floor` here
+        U128((amount.0 as f64 * self.fee_rate as f64 / 100.).round() as u128)
+    }
+}
