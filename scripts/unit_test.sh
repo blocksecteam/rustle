96d46eb273d146227b34ff250810ddd5ac5ba871
xiyao <futuretech6@foxmail.com>
2022-11-09 19:34:41 +0800

    feat: unit-test print result

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -6,7 +6,11 @@ echo $TOP
 echo $DETECTORS_WITH_EXAMPLE
 
 die () {
-    echo -e "\e[31m${*:2}\e[0m" 1>&2  # ]]
+    echo -e "\e[31m${*:2}\e[0m" 1>&2   # ]]
+    echo -e "\e[33mYour result:\e[0m"  # ]]
+    cat $3
+    echo -e "\e[33mExpected result:\e[0m" # ]]
+    cat $4
     exit $1
 }
 
@@ -14,5 +18,6 @@ for detector in $DETECTORS_WITH_EXAMPLE
 do
     $TOP/rustle $TOP/examples/$detector -d $detector
     # cmp --silent $TOP/.tmp/.$detector.tmp $TOP/examples/$detector/expected.txt || die 1 "Unit test fails for the $detector."
-    cmp --silent ./audit-result/summary.csv $TOP/examples/$detector/expected.csv || die 1 "Unit test fails for the $detector."
+    cmp --silent ./audit-result/summary.csv $TOP/examples/$detector/expected.csv \
+        || die 1 "Unit test fails for the $detector." ./audit-result/summary.csv $TOP/examples/$detector/expected.csv
 done
