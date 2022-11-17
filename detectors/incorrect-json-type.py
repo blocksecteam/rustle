40d4a07232ad98ba8e0bc55660498cc960f4c550
xiyao <futuretech6@foxmail.com>
2022-11-17 15:47:11 +0800

    fix: incorrect-json-type includes Option<> type

--- detectors/incorrect-json-type.py
+++ detectors/incorrect-json-type.py
@@ -40,18 +40,25 @@ with open(TMP_PATH + '/.incorrect-json-type.tmp', 'w') as out_file:
             note_issue = ''
 
             typesWITHu128 = ['u128', 'u64', 'Balance', 'Timestamp']
-            for structName, structDetail in structMember_dict.items():
-                for typeWITHu128 in typesWITHu128:
-                    if typeWITHu128 in structDetail.values():
-                        typesWITHu128.append(structName)
-                        break
+            _typesWITHu128_len = len(typesWITHu128) + 1
+
+            while _typesWITHu128_len != len(typesWITHu128):  # repeat until len remains unchanged
+                for structName, structDetail in structMember_dict.items():
+                    for typeWITHu128 in typesWITHu128:
+                        for typeStruct in structDetail.values():
+                            if re.search(r'\b' + typeWITHu128 + r'\b', typeStruct):
+                                typesWITHu128.append(structName)
+                                break
+                _typesWITHu128_len = len(typesWITHu128)
+
             if func['visibility'] == 'public(near_bindgen)':
                 for i in typesWITHu128:
-                    if re.search(r'(:|\s)' + i + r'(,|\s|$)', func['args']):
+                    if re.search(r'(:|\s|<)\b' + i + r'\b(,|\s|>|$)', func['args']):
+                        # if re.search(r'\b' + i + r'\b', func['args']):
                         note_issue += i + ' with too large integer type in public function args, use near_sdk::json_types instead; '
                         break
                 for i in typesWITHu128:
-                    if i == func['return']:
+                    if re.search(r'\b' + i + r'\b', func['return']):
                         note_issue += i + ' with too large integer type in public function return value, use near_sdk::json_types instead; '
                         break
             if note_issue != '':
