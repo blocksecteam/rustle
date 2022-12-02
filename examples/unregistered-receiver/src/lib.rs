2c2b4eab0982b9d54ebb7166510839fb355327db
xiyao <futuretech6@foxmail.com>
2022-12-02 17:38:17 +0800

    feat: add unregistered-receiver example

---
+++ examples/unregistered-receiver/src/lib.rs
@@ -0,0 +1,211 @@
+use near_contract_standards::fungible_token::core::FungibleTokenCore;
+use near_contract_standards::fungible_token::events::FtTransfer;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::json_types::U128;
+use near_sdk::{
+    assert_one_yocto, env, ext_contract, near_bindgen, require, AccountId, Balance,
+    BorshStorageKey, Gas, PromiseOrValue,
+};
+
+const GAS_FOR_RESOLVE_TRANSFER: Gas = Gas(5_000_000_000_000);
+const GAS_FOR_FT_TRANSFER_CALL: Gas = Gas(25_000_000_000_000 + GAS_FOR_RESOLVE_TRANSFER.0);
+
+#[ext_contract(ext_ft_receiver)]
+pub trait FungibleTokenReceiver {
+    fn ft_on_transfer(
+        &mut self,
+        sender_id: AccountId,
+        amount: U128,
+        msg: String,
+    ) -> PromiseOrValue<U128>;
+}
+
+#[ext_contract(ext_ft_resolver)]
+pub trait FungibleTokenResolver {
+    fn ft_resolve_transfer(
+        &mut self,
+        sender_id: AccountId,
+        receiver_id: AccountId,
+        amount: U128,
+    ) -> U128;
+}
+
+#[derive(BorshStorageKey, BorshSerialize)]
+pub enum StorageKey {
+    Accounts,
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    accounts: UnorderedMap<AccountId, Balance>,
+    total_supply: Balance,
+}
+
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            accounts: UnorderedMap::new(StorageKey::Accounts),
+            total_supply: 0,
+        }
+    }
+}
+
+#[near_bindgen]
+impl FungibleTokenCore for Contract {
+    #[payable]
+    fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>) {
+        assert_one_yocto();
+        let sender_id = env::predecessor_account_id();
+        let amount = amount.into();
+        self.internal_transfer_unsafe(&sender_id, &receiver_id, amount, memo); // unsafe, will not panic on unregistered receiver
+    }
+
+    #[payable]
+    fn ft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        amount: U128,
+        memo: Option<String>,
+        msg: String,
+    ) -> PromiseOrValue<U128> {
+        assert_one_yocto();
+        require!(
+            env::prepaid_gas() > GAS_FOR_FT_TRANSFER_CALL,
+            "Insufficient gas"
+        );
+        let sender_id = env::predecessor_account_id();
+        let amount = amount.into();
+
+        self.internal_transfer_safe(&sender_id, &receiver_id, amount, memo); // safe, will panic when receiver is not registered
+        ext_ft_receiver::ext(receiver_id.clone())
+            .with_static_gas(env::prepaid_gas() - GAS_FOR_FT_TRANSFER_CALL)
+            .ft_on_transfer(sender_id.clone(), amount.into(), msg)
+            .then(
+                ext_ft_resolver::ext(env::current_account_id())
+                    .with_static_gas(GAS_FOR_RESOLVE_TRANSFER)
+                    .ft_resolve_transfer(sender_id, receiver_id, amount.into()),
+            )
+            .into()
+    }
+
+    fn ft_total_supply(&self) -> U128 {
+        self.total_supply.into()
+    }
+
+    fn ft_balance_of(&self, account_id: AccountId) -> U128 {
+        U128(
+            self.accounts
+                .get(&account_id.into())
+                .unwrap_or_else(|| env::panic_str("Account is not registered")),
+        )
+    }
+}
+
+impl Contract {
+    pub fn internal_unwrap_balance_safe(&self, account_id: &AccountId) -> Balance {
+        self.accounts
+            .get(account_id)
+            .unwrap_or_else(|| env::panic_str("Account is not registered"))
+    }
+    pub fn internal_unwrap_balance_unsafe(&self, account_id: &AccountId) -> Balance {
+        self.accounts.get(account_id).unwrap_or(0) // shouldn't use a default value for unregistered account according to NEP141
+    }
+
+    pub fn internal_deposit_safe(&mut self, account_id: &AccountId, amount: Balance) {
+        let balance = self.internal_unwrap_balance_safe(account_id);
+
+        if let Some(new_balance) = balance.checked_add(amount) {
+            if new_balance > 0 {
+                self.accounts.insert(account_id, &new_balance);
+            } else {
+                self.accounts.remove(account_id);
+            }
+            self.total_supply = self
+                .total_supply
+                .checked_add(amount)
+                .unwrap_or_else(|| env::panic_str("Total supply overflow"));
+        } else {
+            env::panic_str("Balance overflow");
+        }
+    }
+    pub fn internal_deposit_unsafe(&mut self, account_id: &AccountId, amount: Balance) {
+        let balance = self.internal_unwrap_balance_unsafe(account_id);
+
+        if let Some(new_balance) = balance.checked_add(amount) {
+            if new_balance > 0 {
+                self.accounts.insert(account_id, &new_balance);
+            } else {
+                self.accounts.remove(account_id);
+            }
+            self.total_supply = self
+                .total_supply
+                .checked_add(amount)
+                .unwrap_or_else(|| env::panic_str("Total supply overflow"));
+        } else {
+            env::panic_str("Balance overflow");
+        }
+    }
+
+    pub fn internal_withdraw(&mut self, account_id: &AccountId, amount: Balance) {
+        let balance = self.internal_unwrap_balance_safe(account_id);
+
+        if let Some(new_balance) = balance.checked_sub(amount) {
+            if new_balance > 0 {
+                self.accounts.insert(account_id, &new_balance);
+            } else {
+                self.accounts.remove(account_id);
+            }
+            self.total_supply = self
+                .total_supply
+                .checked_sub(amount)
+                .unwrap_or_else(|| env::panic_str("Total supply insufficient"));
+        } else {
+            env::panic_str("Balance insufficient");
+        }
+    }
+
+    pub fn internal_transfer_safe(
+        &mut self,
+        sender_id: &AccountId,
+        receiver_id: &AccountId,
+        amount: Balance,
+        memo: Option<String>,
+    ) {
+        require!(sender_id != receiver_id, "Identical sender and receiver");
+        require!(amount > 0, "The amount should be a positive number");
+
+        self.internal_withdraw(sender_id, amount);
+        self.internal_deposit_safe(receiver_id, amount);
+
+        FtTransfer {
+            old_owner_id: sender_id,
+            new_owner_id: receiver_id,
+            amount: &U128(amount),
+            memo: memo.as_deref(),
+        }
+        .emit();
+    }
+    pub fn internal_transfer_unsafe(
+        &mut self,
+        sender_id: &AccountId,
+        receiver_id: &AccountId,
+        amount: Balance,
+        memo: Option<String>,
+    ) {
+        require!(sender_id != receiver_id, "Identical sender and receiver");
+        require!(amount > 0, "The amount should be a positive number");
+
+        self.internal_withdraw(sender_id, amount);
+        self.internal_deposit_unsafe(receiver_id, amount);
+
+        FtTransfer {
+            old_owner_id: sender_id,
+            new_owner_id: receiver_id,
+            amount: &U128(amount),
+            memo: memo.as_deref(),
+        }
+        .emit();
+    }
+}
