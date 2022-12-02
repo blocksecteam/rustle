0e5a3b3ece1e0b52a168261b8022077abc7be01d
xiyao <futuretech6@foxmail.com>
2022-12-02 13:52:20 +0800

    feat: add pass of unregistered-receiver

--- Makefile
+++ Makefile
@@ -233,6 +233,16 @@ storage-gas: tg_ir
 	fi
 	@cat ${TMP_DIR}/.bitcodes.tmp | xargs -i $(LLVM_OPT) ${OPTFLAGS} -load detectors/storage_gas.so -storage-gas {} -o /dev/null
 
+unregistered-receiver: tg_ir
+	@rm -f ${TMP_DIR}/.$@.tmp
+	@make -C detectors unregistered_receiver.so
+	@if test $(shell cat ${TMP_DIR}/.bitcodes.tmp | wc -c) -gt 0 ; then \
+		figlet $@ -w 200 ; \
+	else \
+		echo -e "\e[31m[!] Source not found\e[0m" ; \
+	fi
+	@cat ${TMP_DIR}/.bitcodes.tmp | xargs -i $(LLVM_OPT) ${OPTFLAGS} -load detectors/unregistered_receiver.so -unregistered-receiver {} -o /dev/null
+
 tautology:
 	@rm -f ${TMP_DIR}/.$@.tmp
 	@if test $(shell find ${NEAR_SRC_DIR}// -name '*.rs' | wc -c) -gt 0 ; then \
---
+++ detectors/unregistered_receiver.cpp
@@ -0,0 +1,137 @@
+/**
+ * @file self_transfer.cpp
+ * @brief find if ft_transfer checks `sender_id != receiver_id`
+ *
+ */
+#include "near_core.h"
+
+#include <llvm-15/llvm/IR/InstrTypes.h>
+#include <llvm-15/llvm/Support/Casting.h>
+#include <llvm-15/llvm/Support/Regex.h>
+#include <set>
+
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/DebugLoc.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Pass.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Regex.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+namespace {
+    struct UnregisteredReceiver : public llvm::FunctionPass {
+        static char ID;
+
+      private:
+        llvm::raw_fd_ostream *os = nullptr;
+
+        const llvm::Regex regex_ft_transfer_or_call    = llvm::Regex("near_contract_standards..fungible_token..core_impl..FungibleToken\\$.+[0-9]ft_transfer(_call)?[0-9]");
+        const llvm::Regex regex_ft_transfer_trait      = llvm::Regex("near_contract_standards..fungible_token..core..FungibleTokenCore\\$.+[0-9]ft_transfer[0-9]");
+        const llvm::Regex regex_ft_transfer_call_trait = llvm::Regex("near_contract_standards..fungible_token..core..FungibleTokenCore\\$.+[0-9]ft_transfer_call[0-9]");
+
+        const llvm::Regex regex_get              = llvm::Regex("near_sdk[0-9]+collections[0-9]+(LookupMap|TreeMap|UnorderedMap)\\$.+[0-9]+get[0-9]+");
+        const llvm::Regex regex_unchecked_unwrap = llvm::Regex("core[0-9]+(option[0-9]+Option|result[0-9]+Result)\\$.+[0-9]+(unwrap_[a-z_]+)[0-9]+");
+
+      public:
+        UnregisteredReceiver() : FunctionPass(ID) {
+            std::error_code EC;
+            os = new llvm::raw_fd_ostream(std::string(getenv("TMP_DIR")) + std::string("/.unregistered-receiver.tmp"), EC, llvm::sys::fs::OpenFlags::OF_Append);
+        }
+        ~UnregisteredReceiver() { os->close(); }
+
+        bool allowUnregisteredReceiver(llvm::Function *F, llvm::Value *receiver) {
+            using namespace llvm;
+
+            // Use implementation in `near_contract_standards`
+            if (regex_ft_transfer_or_call.match(F->getName()))
+                return true;
+
+            std::set<Value *> usersOfReceiverId;
+            Rustle::simpleFindUsers(receiver, usersOfReceiverId);
+
+            for (auto v : usersOfReceiverId) {
+                if (auto callInst = dyn_cast<CallBase>(v)) {
+                    if (Rustle::isInstCallFunc(callInst, regex_get)) {
+                        std::set<Value *> usersOfGet;
+                        Rustle::findUsers(callInst, usersOfGet);
+
+                        for (auto v : usersOfGet) {  // find if Option of `get` is unwrapped without check
+                            if (auto callInst = dyn_cast<CallBase>(v)) {
+                                if (Rustle::isInstCallFunc(callInst, regex_unchecked_unwrap)) {
+                                    return true;
+                                }
+                            }
+                        }
+                    } else {
+                        int next_receiver_offset = -1;
+                        for (int i = 0; i < callInst->arg_size(); i++) {
+                            if (usersOfReceiverId.count(callInst->getArgOperand(i))) {  // whether passing ft_transfer's arg receiver to next level
+                                next_receiver_offset = i;
+                                break;
+                            }
+                        }
+                        if (next_receiver_offset == -1)  // not found, skip this Instruction
+                            continue;
+
+                        if (callInst->getCalledFunction() && allowUnregisteredReceiver(callInst->getCalledFunction(), callInst->getCalledFunction()->getArg(next_receiver_offset))) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        bool runOnFunction(llvm::Function &F) override {
+            using namespace llvm;
+            if (!Rustle::debug_check_all_func && Rustle::regexForLibFunc.match(F.getName()))
+                return false;
+            if (Rustle::debug_print_function)
+                Rustle::Logger().Debug("Checking function ", F.getName());
+
+            if (regex_ft_transfer_trait.match(F.getName())) {
+                if (F.arg_size() < 3)
+                    return false;
+
+                Rustle::Logger().Info("Find ft_transfer \e[34m", F.getName());
+                *os << F.getName();
+
+                if (allowUnregisteredReceiver(&F, F.getArg(1))) {
+                    Rustle::Logger().Warning("Unregistered receiver can be used for \e[34mft_transfer");
+                    *os << "@False\n";
+                } else {
+                    Rustle::Logger().Info("Unregistered receiver can't be used for \e[34mft_transfer");
+                    *os << "@True\n";
+                }
+            } else if (regex_ft_transfer_call_trait.match(F.getName())) {
+                if (F.arg_size() < 3)
+                    return false;
+
+                Rustle::Logger().Info("Find ft_transfer_call \e[34m", F.getName());
+                *os << F.getName();
+
+                if (allowUnregisteredReceiver(&F, F.getArg(2))) {
+                    Rustle::Logger().Warning("Unregistered receiver can be used for3 \e[34mft_transfer_call");
+                    *os << "@False\n";
+                } else {
+                    Rustle::Logger().Info("Unregistered receiver can't be used for \e[34mft_transfer_call");
+                    *os << "@True\n";
+                }
+            }
+
+            return false;
+        }
+    };
+}  // namespace
+
+char UnregisteredReceiver::ID = 0;
+static llvm::RegisterPass<UnregisteredReceiver> X("unregistered-receiver", "", false /* Only looks at CFG */, false /* Analysis Pass */);
+
+static llvm::RegisterStandardPasses Y(
+    llvm::PassManagerBuilder::EP_EarlyAsPossible, [](const llvm::PassManagerBuilder &Builder, llvm::legacy::PassManagerBase &PM) { PM.add(new UnregisteredReceiver()); });
